95aa71b387fea05345c5f5e52a8d751b22f359e4
Israel Cohen <israelcohen54@gmail.com>
2020-12-10 20:58:13 +0200

    shorten two func', edited a bit some comments

--- minCircle.h
+++ minCircle.h
@@ -1,21 +1,17 @@
-// ID1	ID2
+// 205812290	ID2
 
 #ifndef MINCIRCLE_H_
 #define MINCIRCLE_H_
-
 #include <iostream>
-
 //----Testing---
 #include <algorithm>
 #include <assert.h>
 #include <iostream>
 #include <math.h>
 #include <vector>
-////
+//
 using namespace std;
-
 // ------------ DO NOT CHANGE -----------
-
 class Point{
 public:
 	float x,y;
@@ -32,38 +28,17 @@ public:
 /*
 // implement
 Circle findMinCircle(Point** points,size_t size){
-
 	return Circle(Point(0,0),0);
 }
 */
 // you may add helper functions here
-
 //-------------------------TESTING--------------------------------
-
 
 // Defining infinity
 const float INF = 1e18;
 
-// Function to return the euclidean distance
-// between two points
-float dist(const Point& a, const Point& b) //SHORTEN, put in the right place
-{
-    return sqrt(pow(a.x - b.x, 2)
-                + pow(a.y - b.y, 2));
-}
-
-// Function to check whether a point lies inside
-// or on the boundaries of the circle
-bool is_inside(const Circle& c, const Point& p) //SHORTEN, put if in the right place
-{
-    return dist(c.center, p) <= c.radius;
-}
-
-// The following two functions are used
-// To find the equation of the circle when
-// three points are given.
-
-// Helper method to get a circle defined by 3 points
+// **The following two functions are used to find the equation of the circle with three given points**
+// Method to get a circle defined by 3 points, as it a critical part of the solution:
 Point get_circle_center(float bx, float by,
                         float cx, float cy)
 {
@@ -73,42 +48,39 @@ Point get_circle_center(float bx, float by,
     return { (cy * B - by * C) / (2 * D),
              (bx * C - cx * B) / (2 * D) };
 }
-
 // Function to return a unique circle that
 // intersects three points
-Circle circle_from(const Point& A, const Point& B,
-                   const Point& C)
+Circle circle_from(const Point& A, const Point& B,const Point& C)
 {
     Point I = get_circle_center(B.x - A.x, B.y - A.y,
                                 C.x - A.x, C.y - A.y);
-
     I.x += A.x;
     I.y += A.y;
-    return { I, dist(I, A) };
+    //using euclidean distance in the right side of the return function line below
+    return { I, float(sqrt(pow(I.x - A.x, 2) + pow(I.y - A.y, 2))) };
 }
-
 // Function to return the smallest circle
 // that intersects 2 points
 Circle circle_from(const Point& A, const Point& B)
 {
-    // Set the center to be the midpoint of A and B
+    // Set the center to be the midpoint of A and B:
     Point C = { (A.x + B.x) / 2.f, (A.y + B.y) / 2.f };
 
-    // Set the radius to be half the distance AB
-    return { C, dist(A, B) / 2.f };
+    // Set the radius to be half the distance AB, doing that with
+    // using euclidean distance in the right side of the return function line below:
+    return { C, float(sqrt(pow(A.x - B.x, 2) + pow(A.y - B.y, 2))) / 2.f };
 }
 
-// Function to check whether a circle
-// encloses the given points
-bool is_valid_circle(const Circle& c,
-                     const vector<Point>& points_on_boundary)
+// Function to check whether a circle encloses the given points:
+bool is_valid_circle(const Circle& c, const vector<Point>& points_on_boundary)
 {
 
-    // Iterating through all the points
-    // to check  whether the points
-    // lie inside the circle or not
+    // Iterating through all the points to check whether the
+    // points inside the circle bounderies (include the bounderies themselves) or not:
     for (const Point& p : points_on_boundary)
-        if (!is_inside(c, p))
+        if ((sqrt(pow(c.center.x - p.x, 2)
+         + pow(c.center.y - p.y, 2)) <= c.radius)==false) //checking if point inside bounderies
+                                                        //(include the bounderies themselves), if not, than - 
             return false;
     return true;
 }
@@ -139,35 +111,29 @@ Circle min_circle_trivial(vector<Point>& points_on_boundary)
     }
     return circle_from(points_on_boundary[0], points_on_boundary[1], points_on_boundary[2]);
 }
+/*
+ We are Returning the MEC with the Welzl's algorithm.
+ It takes a set of input points P and a set R points on the circle boundary. 
+ n representing the number of points in P that are not yet processed.
 
-// Returns the MEC using Welzl's algorithm
-// Takes a set of input points P and a set R
-// points on the circle boundary.
-// n represents the number of points in P
-// that are not yet processed.
-
-//Circle welzl_helper(vector<Point>& P,vector<Point> R, int n)
+Circle welzl_helper(vector<Point>& P,vector<Point> R, int n) */
 Circle welzlAlgorithm(Point** points,vector<Point> points_on_boundary, size_t size) {
     // Base case when all points processed or |R| = 3
     if (size == 0 || points_on_boundary.size() == 3) {
         return min_circle_trivial(points_on_boundary);
     }
-
     // Pick a random point randomly
     int random_index = rand() % size;
     Point p = *points[random_index];
 
-    // Put the picked point at the end of P
-    // since it's more efficient than
-    // deleting from the middle of the vector
+    // Put the picked point at the end of P as it's more efficient than deleting from the middle of the vector
     swap(*points[random_index], *points[size - 1]);
 
-    // Get the MEC circle d from the
-    // set of points P - {p}
+    // Get the MEC circle d from the set of points P - {p}
     Circle MEC = welzlAlgorithm(points, points_on_boundary, size - 1);
 
-    //If d contains p, return d
-    if (is_inside(MEC, p)) {
+    //If d contains p, return d, meaning if inside (boundry *), line below calc oclid dis between thet point and the radius, if lesser that, its mean its within bounderies
+    if ((sqrt(pow(MEC.center.x - p.x, 2) + pow(MEC.center.y - p.y, 2)) <= MEC.radius)==true) {
         return MEC;
     }
 
@@ -184,6 +150,4 @@ Circle findMinCircle(Point** points, size_t size)
    // random_shuffle(*points[0], *points[size - 1]);
     return welzlAlgorithm(points, {}, size);
 }
-
-
 #endif /* MINCIRCLE_H_ */
